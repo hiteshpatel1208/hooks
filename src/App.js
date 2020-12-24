b23d06c5462d992e34c6b70ad5a37b6819f35e6e
hitesh.patel <hitesh.c.patel@capgemini.com>
2020-12-24 16:53:15 -0500

    useState updated

--- src/App.js
+++ src/App.js
@@ -14,14 +14,14 @@ function App() {
   }
 
   return (
-    <>
+    <div className="App">
       <div style={{display: 'flex', justifyContent: 'center'}}>
         {
           hookTypes.map((hookType, ndx) => {
             return (
               <div key={ndx} style={{margin: 10, border: '1px solid black', padding: 10}} >
                 <label style={{cursor: 'grab'}}>
-                  <input style={{}} type="radio" name="hook" value={`${hookType}`} onChange={(e) => onRadioSelect(e.target.value)} />
+                  <input type="radio" name="hook" value={`${hookType}`} onChange={(e) => onRadioSelect(e.target.value)} />
                   <span style={{marginLeft: 10, marginRight: 10}}>{hookType}</span>
                 </label>
               </div>
@@ -32,7 +32,7 @@ function App() {
       <div style={{marginTop: 50, padding: 20}}>
         {comp}
       </div>
-    </>
+    </div>
   );
 }
 
--- src/components/useState/useStateHook.js
+++ src/components/useState/useStateHook.js
@@ -1,11 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Usestatehook(props) {
-    
+    const [count, setCount] = useState(0);
+
+    const initialCount = 0;
+    const [count2, setCount2] = useState(initialCount);
+
+    const [name, setName] = useState({ fName: '', lname: '' });
+
+    const [items, setItems] = useState([]);
+
+    const addItem = () => {
+        setItems([...items, { id: items.length, value: Math.floor(Math.random() * 10) + 1 }])
+    }
 
     return (
         <>
-            This is useState Hook.
+            <h1>This is useState Hook.</h1>
+            <div className="hook-container">
+                <div className="hook-subcontainer">
+                    <h4>useState with simple increment counter</h4>
+                    <p>Counter 1: {count}</p>
+                    <button onClick={() => setCount(count + 1)}>Increment</button>
+                </div>
+                <div className="hook-subcontainer">
+                    <h4>useState with increment/decrement/reset counter that using prevState as function to setter function</h4>
+                    <p>Counter 2: {count2}</p>
+                    <button onClick={() => setCount2(initialCount)}>Reset</button>
+                    <button onClick={() => setCount2(prevCount2 => prevCount2 + 1)}>Increment</button>
+                    <button onClick={() => setCount2(prevCount2 => prevCount2 - 1)}>Descrement</button>
+                </div>
+                <div className="hook-subcontainer">
+                    <h4>useState with Object</h4>
+                    <p>First Name: {name.fName}</p>
+                    <p>Last Name: {name.lName}</p>
+                    <form>
+                        <input type="text" value={name.fName} onChange={e => setName({ ...name, fName: e.target.value })} />
+                        <input type="text" value={name.lName} onChange={e => setName({ ...name, lName: e.target.value })} />
+                    </form>
+                </div>
+                <div className="hook-subcontainer">
+                    <h4>useState with Array</h4>
+                    <ul>
+                        {
+                            items.map(item => <li key={item.id}>{item.value}</li>)
+                        }
+                    </ul>
+                    <button onClick={addItem}>Add a number</button>
+                </div>
+            </div>
         </>
     );
 }
